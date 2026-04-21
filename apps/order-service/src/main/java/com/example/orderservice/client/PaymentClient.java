41d755e2645f15fb533ca131e050499574d452ec
jw-django <jung77dab@gmail.com>
2026-04-21 11:04:50 +0900

    fix: client svc misspell

--- apps/order-service/src/main/java/com/example/orderservice/client/PaymentClient.java
+++ apps/order-service/src/main/java/com/example/orderservice/client/PaymentClient.java
@@ -3,7 +3,7 @@ package com.example.orderservice.client;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 
-@FeignClient(name = "payment-client", url = "http://payment-servce:8080")
+@FeignClient(name = "payment-client", url = "http://payment-service:8080")
 public interface PaymentClient {
 
     @PostMapping("/api/payments")
